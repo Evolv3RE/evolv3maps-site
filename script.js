d053bfc98b1cf7c0dbab82a9082c176ca25fdd9f
Evolv3RE <michael@evolve-realestate.com>
2025-10-13 15:48:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+mapboxgl.accessToken="pk.eyJ1IjoiZXZvbHZlMSIsImEiOiJjbWQzZWwzZW4wNDB6MmpxNGl3dGNqODBkIn0.s-8k7OStBDRIC7SSFb6wKQ";
+const map=new mapboxgl.Map({container:"map",style:"mapbox://styles/mapbox/dark-v11",center:[-97.7431,30.2672],zoom:9});
