f510ac8b0ad6dfa33387311d102357457568138a
Michael Gonzalez <michael@evolve-realestate.com>
2025-10-30 14:52:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -108,11 +108,11 @@ if (!window.mapboxgl) {
 
 // Load power infrastructure scripts
 var s1 = document.createElement('script');
-s1.src = 'public/scripts/corridor_style.js';
+s1.src = './scripts/corridor_style.js';
 document.body.appendChild(s1);
 var s2 = document.createElement('script');
-s2.src = 'public/scripts/voltage_filter_ui.js';
+s2.src = './scripts/voltage_filter_ui.js';
 document.body.appendChild(s2);
 var s3 = document.createElement('script');
-s3.src = 'public/scripts/power_layerset.js';
+s3.src = './scripts/power_layerset.js';
 document.body.appendChild(s3);
